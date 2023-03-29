53986050a962aeceab7a62248440faa1deb6e103
biscuitmer <enzo35500@outlook.fr>
2023-03-29 11:52:21 +0200

    autentification

--- projet_tu/server.js
+++ projet_tu/server.js
@@ -1,7 +1,16 @@
 const express = require('express');
-const app = express();
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('mydatabase.db');
+
+const app = express();
+const port = 3000;
+
+// Création de la base de données et de la table "users" avec deux utilisateurs pré-enregistrés
+const db = new sqlite3.Database(':memory:');
+db.serialize(() => {
+  db.run('CREATE TABLE users (id INTEGER PRIMARY KEY, username TEXT UNIQUE NOT NULL, password TEXT NOT NULL)');
+  db.run('INSERT INTO users (username, password) VALUES (?, ?)', ['john', 'password123']);
+  db.run('INSERT INTO users (username, password) VALUES (?, ?)', ['jane', 'abc123']);
+});
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
@@ -13,26 +22,23 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
-  // Vérifiez ici si l'utilisateur est authentifié
-  res.send(`Bienvenue, ${username} !`);
+
+  // Vérifiez si l'utilisateur existe dans la base de données
+  db.get('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], (err, row) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send('Erreur serveur');
+    } else if (row) {
+      // Si l'utilisateur existe, affichez un message de bienvenue
+      res.send(`Bienvenue, ${username}`)
+        } else {
+      // Sinon, affichez un message d'erreur
+      res.status(401).send('Nom d\'utilisateur ou mot de passe incorrect');
+    }
+  });
 });
 
 app.listen(3000, () => {
   console.log('Le serveur est démarré: http://localhost:3000/');
 });
 
-
-
-db.run('CREATE TABLE users (id INTEGER PRIMARY KEY, username TEXT, password TEXT)');
-db.run('INSERT INTO users (username, password) VALUES (?, ?)', ['john', 'password123']);
-db.run('INSERT INTO users (username, password) VALUES (?, ?)', ['jane', 'abc123']);
-
-db.all('SELECT * FROM users', [], (err, rows) => {
-  if (err) {
-    throw err;
-  }
-  rows.forEach(row => {
-    console.log(row.username, row.password);
-  });
-});
-db.close()
