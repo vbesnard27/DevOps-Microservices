0f54b5101abc68f6b7480c5426e8dfd8667ccf05
vbesnard <valentin.besnard@etudiant.univ-rennes1.fr>
2023-03-29 11:52:12 +0200

    ajout d'exemple dans la base de données

--- projet_tu/database.sql
+++ projet_tu/database.sql
@@ -16,3 +16,21 @@ CREATE TABLE schools (
   degree VARCHAR(100) NOT NULL,
   ects_credits INT
 );
+
+INSERT INTO users (id, username, password, email, registration_date)
+VALUES
+  (1, 'johndoe', 'mypassword', 'johndoe@example.com', '2022-01-01'),
+  (2, 'janedoe', 'janepassword', 'janedoe@example.com', '2022-02-15'),
+  (3, 'bobsmith', 'bobpassword', 'bobsmith@example.com', '2022-03-23'),
+  (4, 'alicewonder', 'alicepassword', 'alicewonder@example.com', '2022-04-30'),
+  (5, 'tomjones', 'tompasword', 'tomjones@example.com', '2022-05-15');
+
+INSERT INTO schools (id, name, type, address, degree, ects_credits)
+VALUES
+  (1, 'Université de Paris', 'Université', 'Paris, France', 'Licence en Mathématiques', 180),
+  (2, 'École polytechnique', 'Grande école', 'Palaiseau, France', 'Diplôme d\''ingénieur', 300),
+  (3, 'Sorbonne Université', 'Université', 'Paris, France', 'Master en Histoire', 120),
+  (4, 'HEC Paris', 'Grande école', 'Jouy-en-Josas, France', 'Master en Management', 120),
+  (5, 'École normale supérieure de Lyon', 'Grande école', 'Lyon, France', 'Doctorat en Biologie', 480);
+
+
